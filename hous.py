75102828c60160ce3a653dcdd0a4ed3f0a6b2a74
Sjmavrik <sjmavrik@gmail.com>
2021-04-19 00:09:12 +0300

    Реализовал функцию draw_house
    Придумал ее реализацю.
    Нужно сделать еще три функции: для
    основния, стен, крыши.

--- hous.py
+++ hous.py
@@ -1,3 +1,10 @@
+def main():
+    x, y = 200, 300
+    width, heigth = 200, 300
+
+    drow_house(x, y, width, heigth)
+
+
 def drow_house(x, y, width, heigth):
     """
     фуркция рисует домик ширины width и высоты height от опортной точки (х, у)
@@ -8,10 +15,36 @@ def drow_house(x, y, width, heigth):
     height: полная высота домика
     """
     print('рисую домик')
-    pass  # do noting
+    foundation_heigth = 0.05 * heigth
+    walls_width = 0.9 * width
+    walls_heigth = 0.5 * heigth
+    roof_heigth = heigth - foundation_heigth - walls_heigth
+
+    draw_house_foundation(x, y, width, foundation_heigth)
+    draw_house_walls(x, y - foundation_heigth, walls_width, walls_heigth)
+    draw_house_roof(x, y - foundation_heigth - walls_heigth, width, roof_heigth)
 
 
-x, y = 100, 100
-width, heigth = 200, 200
+def draw_house_foundation(x, y, width, heigth):
+    """
+    Рисование основание домика ширины width  и высоты height от опорной точки (x, y),
+    которая находится в середине нижней точки фундамента.
+    х: координата середины фундамента
+    у: координата низа фундамента
+    width: полная ширина фундамента
+    height: полная высота фундамента
+    """
+    print('рисую основание')
+    pass
 
-drow_house(x, y, width, heigth)
+
+def draw_house_walls(x, y , walls_width, heigth):
+    print('рисую стены')
+    pass
+
+
+def draw_house_roof(x, y , width, heigth):
+    print('рисую крышу')
+    pass
+
+main()
